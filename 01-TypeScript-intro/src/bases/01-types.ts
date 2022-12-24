7837c9a5fa1f9da71a7e154c3839792450c98a2d
romerodevelopments <gandresrp@gmail.com>
2022-12-23 23:34:12 -0300

    Breve introduccion a typescript y conocimientos generales necesarios - Tipos de datos continuacion

--- 01-TypeScript-intro/src/bases/01-types.ts
+++ 01-TypeScript-intro/src/bases/01-types.ts
@@ -1,3 +1,16 @@
 export const name: string = 'Gustavo'
 export const age: number = 32;
 export const isValid: boolean = true;
+
+export const templateString = ` Esto es un string
+multilinea
+que puede tener
+" dobles
+' simple
+inyectar valores ${ name }
+expresiones ${ 1 + 1 }
+números: ${ age }
+booleanos: ${ isValid }
+`
+
+console.log( templateString );
