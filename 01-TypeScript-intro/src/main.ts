37540afb321a98a87b88a0c31a6244236652cad5
romerodevelopments <gandresrp@gmail.com>
2022-12-24 18:47:29 -0300

    Breve introduccion a typescript y conocimientos generales necesarios - clases y forma abreviada

--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import { setupCounter } from './counter'
 
-import { name, age } from './bases/01-types'
+// import { name, age } from './bases/01-types'
+import { charmander } from './bases/03-classes'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -12,7 +13,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Hello ${name} ${age}</h1>
+    <h1>Hello ${charmander.name}</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
