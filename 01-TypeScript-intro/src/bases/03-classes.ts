f3db6bdb91d40db1d4decedecf2c579b855b06a0
romerodevelopments <gandresrp@gmail.com>
2022-12-29 17:43:55 -0300

    Breve introduccion a typescript y conocimientos generales necesarios - Getters, metodos y THIS

--- 01-TypeScript-intro/src/bases/03-classes.ts
+++ 01-TypeScript-intro/src/bases/03-classes.ts
@@ -3,6 +3,21 @@ export class Pokemon {
     public readonly id: number,
     public name: string,
   ) { }
+
+  get imageUrl(): string {
+    return `https://pokemon.com/${this.id}.jpg`;
+  }
+
+  scream() {
+    console.log(`${this.name.toUpperCase()}!!!`);
+  }
+
+  speak() {
+    console.log(`${this.name}, ${this.name}`);
+  }
 }
 
-export const charmander = new Pokemon( 4, 'Charmander' );
+export const charmander = new Pokemon(4, 'Charmander');
+
+charmander.scream();
+charmander.speak();
