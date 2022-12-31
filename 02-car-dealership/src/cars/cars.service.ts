1e0b71477bb0fad11d35ba93042937ed443076f4
romerodevelopments <gandresrp@gmail.com>
2022-12-31 20:14:58 -0300

    Primeros pasos en Nest - Exception filters

--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -25,7 +25,10 @@ export class CarsService {
   }
 
   findOneById(id: number) {
+
     const car = this.cars.find(car => car.id === id);
+    if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
+
     return car;
   }
 
