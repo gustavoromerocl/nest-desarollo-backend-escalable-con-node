f5e9e2c713d5a77f462fdd67348ff4ef89074bd6
romerodevelopments <gandresrp@gmail.com>
2022-12-30 22:07:46 -0300

    Breve introduccion a typescript y conocimientos generales necesarios - decorador de metodo

--- 01-TypeScript-intro/src/main.ts
+++ 01-TypeScript-intro/src/main.ts
@@ -4,7 +4,7 @@ import { setupCounter } from './counter'
 
 // import { name, age } from './bases/01-types'
 // import { charmander } from './bases/03-classes'
-import { charmander } from './bases/05-decorators'
+import { charmander } from './bases/06-decorators'
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
