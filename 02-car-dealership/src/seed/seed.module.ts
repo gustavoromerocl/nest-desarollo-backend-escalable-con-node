42b0574e595cb38d9aee2aba2fdd9af0377d4537
romerodevelopments <gandresrp@gmail.com>
2023-01-06 08:28:40 -0300

    Nest CLI Resource - Brands CRUD - Inyectar servicios en otros servicios

--- 02-car-dealership/src/seed/seed.module.ts
+++ 02-car-dealership/src/seed/seed.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { CarsModule } from '../cars/cars.module';
+import { BrandsModule } from '../brands/brands.module';
 
 @Module({
+  imports: [
+    CarsModule,
+    BrandsModule
+  ],
   controllers: [SeedController],
   providers: [SeedService]
 })
