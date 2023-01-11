76c316cd5d3810762fefcdbf5f0c9917fbea1a26
romerodevelopments <gandresrp@gmail.com>
2023-01-11 08:29:13 -0300

    MongoDB Pokedex - POST recibir y validar la data

--- 03-pokedex/src/main.ts
+++ 03-pokedex/src/main.ts
@@ -1,9 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
   //Establece el path por defaul de todos los endpoints
   app.setGlobalPrefix('api/v2');
 
