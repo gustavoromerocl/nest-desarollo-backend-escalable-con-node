f624f1c5e0142a6580e537a08dae48c621d0e879
romerodevelopments <gandresrp@gmail.com>
2023-01-11 21:15:47 -0300

    MongoDB Pokedex - findOneBy: Buscar por nombre, MongoId y no

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -1,6 +1,6 @@
-import { BadGatewayException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadGatewayException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
@@ -11,18 +11,18 @@ export class PokemonService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>
-  ){}
+  ) { }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
 
     try {
-      const pokemon = await this.pokemonModel.create( createPokemonDto )
+      const pokemon = await this.pokemonModel.create(createPokemonDto)
       return pokemon;
-      
+
     } catch (error) {
       if (error.code === 11000) {
-        throw new BadGatewayException(`Pokemon exist in db ${ JSON.stringify(error.keyValue) }`);
+        throw new BadGatewayException(`Pokemon exist in db ${JSON.stringify(error.keyValue)}`);
       }
       console.log(error);
       throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`)
@@ -33,8 +33,26 @@ export class PokemonService {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+    let pokemon: Pokemon;
+
+    if (!isNaN(+term)) {
+      pokemon = await this.pokemonModel.findOne({ no: term })
+    }
+
+
+    //MongoId
+    if (!pokemon && isValidObjectId(term)) {
+      pokemon = await this.pokemonModel.findById(term);
+    }
+
+    //Name
+    if (!pokemon)
+      pokemon = await this.pokemonModel.findOne({ name: term.toLowerCase().trim() })
+
+    if (!pokemon) throw new NotFoundException(`Pokemon with id, name or no "${term}" not found`);
+
+    return pokemon
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
