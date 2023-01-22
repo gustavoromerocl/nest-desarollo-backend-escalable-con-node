0f9a74a609d37e0cd14451a50a87da6c17e51542
romerodevelopments <gandresrp@gmail.com>
2023-01-22 18:48:32 -0300

    Seed y paginacion - Insertar multiples registros simultaneamente

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -4,28 +4,56 @@ import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
 // import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
-import { PokemonService } from '../pokemon/pokemon.service';
+// import { PokemonService } from '../pokemon/pokemon.service';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
   constructor(
-    // @InjectModel(Pokemon.name)
-    // private readonly pokemonModel: Model<Pokemon>
-    private readonly pokemonService: PokemonService,
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    // private readonly pokemonService: PokemonService,
   ) { }
 
   
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+    await this.pokemonModel.deleteMany({}); //delete * from pokemon
     
-    data.results.forEach(async ({ name, url }) => {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+    
+    //Insertar multiples registros simultaneamente
+
+    //Forma 1
+    const pokemonToInsert: {name: string, no: number}[] = [];
+    
+    data.results.forEach(({ name, url }) => {
 
       const segments = url.split('/');
       const no = +segments[ segments.length - 2 ];
-      const pokemon = await this.pokemonService.create({name, no});
+      // const pokemon = await this.pokemonService.create({name, no});
+      pokemonToInsert.push({name, no});
     });
+
+    this.pokemonModel.insertMany(pokemonToInsert);
+    
+    //Forma 2
+
+    // const insertPromisesArray = [];
+    
+    // data.results.forEach(({ name, url }) => {
+
+    //   const segments = url.split('/');
+    //   const no = +segments[ segments.length - 2 ];
+    //   // const pokemon = await this.pokemonService.create({name, no});
+    //   insertPromisesArray.push(this.pokemonService.create({name, no}));
+    // });
+
+
+    // await Promise.all( insertPromisesArray );
+    
+    
 
     return 'Seed executed'
   }
