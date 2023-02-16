41c86c40ce0e588f256e9ad73b83a71dbe8df5ac
romerodevelopments <gandresrp@gmail.com>
2023-02-16 13:05:06 -0300

    TypeORM: Postgres - QueryBuilder

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -37,13 +37,17 @@ export class ProductsService {
 
   async findOne(term: string) {
     let product: Product;
-    // const product = await this.productRepository.findOneBy({ id });
-    if(isUUID(term)) {
-      product = await this.productRepository.findOneBy({id:term});
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term });
     } else {
-      product = await this.productRepository.findOneBy({slug:term});
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where(`UPPER(title) = :title or slug = :slug`, {
+          title: term.toUpperCase(),
+          slug: term.toLocaleLowerCase(),
+        }).getOne();
     }
-    
+
     if (!product)
       throw new NotFoundException(`Product with id ${term} not found`);
 
