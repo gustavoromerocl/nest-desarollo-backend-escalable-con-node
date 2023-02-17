68a2b22be3af4b244c171922aa597530bcc7d8a3
romerodevelopments <gandresrp@gmail.com>
2023-02-16 23:01:11 -0300

    TypeORM: Postgres - Update en TypeORM

--- 04-teslo-shop/src/products/products.controller.ts
+++ 04-teslo-shop/src/products/products.controller.ts
@@ -6,7 +6,7 @@ import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
@@ -20,17 +20,19 @@ export class ProductsController {
   }
 
   @Get(':term')
-  findOne(@Param('term', ) term: string) {
+  findOne(@Param('term',) term: string) {
     return this.productsService.findOne(term);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
   remove(@Param('id', ParseUUIDPipe) id: string) {
-    return this.productsService.remove( id);
+    return this.productsService.remove(id);
   }
 }
