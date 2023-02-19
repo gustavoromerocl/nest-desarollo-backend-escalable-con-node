38f0167d4717410ad7b6ca185d3e9b3281084527
romerodevelopments <gandresrp@gmail.com>
2023-02-19 19:59:17 -0300

    Carga de archivos - Validar archivos

---
+++ 04-teslo-shop/src/files/helpers/fileFilter.helper.ts
@@ -0,0 +1,16 @@
+export const fileFiler = (
+  req: Express.Request, 
+  file: Express.Multer.File, 
+  callback: Function) => {
+    console.log({file})
+
+    if( !file ) return callback( new Error('File is empty'), false);
+    const fileExtension = file.mimetype.split('/')[1];
+    const validaExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+
+    if(validaExtensions.includes(fileExtension)) {
+      return callback ( null, true );
+    }
+
+    callback(null, true);
+}
