ed4ad8ed94b4139a4c22e53996d7ebf6952cdf60
romerodevelopments <gandresrp@gmail.com>
2023-02-19 20:42:13 -0300

    Carga de archivos - Guardar imagen en FileSystem

--- 04-teslo-shop/src/files/files.controller.ts
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileFiler } from './helpers/fileFilter.helper';
 
@@ -9,15 +10,20 @@ export class FilesController {
   constructor(private readonly filesService: FilesService) { }
 
   @Post('product')
-  @UseInterceptors( FileInterceptor('file', {
-    fileFilter: fileFiler
-  }) )
+  @UseInterceptors(FileInterceptor('file', {
+    fileFilter: fileFiler,
+    // limits: { fileSize: 1000 }
+    storage: diskStorage({
+      destination: './static/uploads'
+    })
+  }))
   uploadProductImage(
     @UploadedFile() file: Express.Multer.File
   ) {
 
-    if( !file ) throw new BadRequestException('Make sure that file is an image');
-    
+    if (!file)
+      throw new BadRequestException('Make sure that file is an image');
+
     return {
       fileName: file.originalname
     };
