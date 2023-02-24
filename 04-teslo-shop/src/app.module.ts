b92c02db5274f4e1f1278cba20b39f380d451205
romerodevelopments <gandresrp@gmail.com>
2023-02-24 17:52:07 -0300

    Autenticacion de autorizacion - Crear usuario

--- 04-teslo-shop/src/app.module.ts
+++ 04-teslo-shop/src/app.module.ts
@@ -24,9 +24,9 @@ import { AuthModule } from './auth/auth.module';
       autoLoadEntities: true,
       synchronize: true
     }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname,'..','public'),
-    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname,'..','public'),
+    // }),
     ProductsModule,
     CommonModule,
     SeedModule,
