bc4a462fbc2473776066c87dda8d8c83e9372c1e
romerodevelopments <gandresrp@gmail.com>
2023-02-26 23:42:03 -0300

    Autenticacion de autorizacion - SEED de usuarios, productos e imagenes

--- 04-teslo-shop/src/seed/seed.service.ts
+++ 04-teslo-shop/src/seed/seed.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/entities/user.entity';
+import { Repository } from 'typeorm';
 import { ProductsService } from '../products/products.service';
 import { initialData } from './data/seed-data';
 
@@ -6,26 +9,54 @@ import { initialData } from './data/seed-data';
 export class SeedService {
   constructor(
     private readonly productService: ProductsService,
+    
+    @InjectRepository( User )
+    private readonly userRepository: Repository<User>
   ) { }
 
   async runSeed() {
-    await this.insertNewProducts();
-
-    const products = initialData.products;
-
-    const insertPromises = [];
-
-    // products.forEach( product => {
-    //   insertPromises.push(this.productService.create( product ))
-    // });
-
-    await Promise.all(insertPromises);
+    await this.deleteTables();
+    const adminUser = await this.insertUsers();
+    await this.insertNewProducts( adminUser );
 
     return "SEED EXECUTED"
   }
 
-  private async insertNewProducts() {
-    await this.productService.deleteAllProducts();
+  private async insertNewProducts(user: User) {
+    const products = initialData.products;
+
+    const insertPromises = [];
+
+    products.forEach( product => {
+      insertPromises.push(this.productService.create( product, user ))
+    });
+
+    await Promise.all(insertPromises);
     return true;
+  }
+
+  private async insertUsers() {
+    const seedUsers = initialData.users;
+
+    const users: User[] = [];
+
+    seedUsers.forEach( user => {
+      users.push( this.userRepository.create( user ));
+    })
+
+    const dbUsers = await this.userRepository.save( seedUsers );
+
+    return dbUsers[0]
+  }
+
+  private async deleteTables() {
+    await this.productService.deleteAllProducts();
+
+    const queryBuilder = this.userRepository.createQueryBuilder();
+
+    await queryBuilder
+      .delete()
+      .where({})
+      .execute()
   }
 }
