dca94a3b8530f7bf1d578f2c95f70ccfd97ee221
romerodevelopments <gandresrp@gmail.com>
2023-02-26 22:17:42 -0300

    Autenticacion de autorizacion - Auth en otros modulos

--- 04-teslo-shop/src/seed/seed.module.ts
+++ 04-teslo-shop/src/seed/seed.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { ProductsModule } from '../products/products.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [ProductsModule]
+  imports: [
+    ProductsModule,
+    //Al importar el modulo completo, se importan todos los imports que este posee, en este caso las dependencias nececsarias para ejecutar el sistema de autorizacion
+    AuthModule
+  ]
 })
 export class SeedModule {}
